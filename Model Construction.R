6e4189b8e7b85b5215edbb04284ad6f9da31b78f
jbw5332 <31397809+jbw5332@users.noreply.github.com>
2018-11-06 13:13:22 -0500

    Add files via upload

--- Model Construction.R
+++ Model Construction.R
@@ -1,8 +1,30 @@
 #Wiki Mobile
+plot(wikiMobileViews)
+mod = estimate(AR(1), wikiMobileViews)
+check(mod)
 
-#Desktop Mobile
+mod = estimate(AR(15), wikiMobileViews)
+check(mod)
+
+select(AR(15), wikiMobileViews, include.mean = TRUE)
+mobile_prediction = predict(mod, n.ahead = 1)
+
+mobile_mape = MAPE(AR(15), wikiMobileViews, start = 0.8)
+
+mobile_prediction = predict(mod, n.ahead = 1)
+
+#Wiki Desktop
+plot(wikiDesktopViews)
+mod1 = estimate(AR(1), wikiDesktopViews)
+check(mod1)
+
+mod1 = estimate(AR(7), wikiDesktopViews)
+check(mod1)
 
 #Silvio
+plot(ArticleSilvioViews)
+mod2 = estimate(AR(7), ArticleSilvioViews)
+check(mod2)
 
 #Beyonce
 
