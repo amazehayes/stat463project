c69b3e6de518db9648740dd8c67d3f05cd8e6327
jbw5332 <31397809+jbw5332@users.noreply.github.com>
2018-11-06 14:00:45 -0500

    Add files via upload

--- Model Construction.R
+++ Model Construction.R
@@ -23,13 +23,50 @@ check(mod1)
 
 #Silvio
 plot(ArticleSilvioViews)
-mod2 = estimate(AR(7), ArticleSilvioViews)
+mod2 = estimate(AR(1), ArticleSilvioViews)
 check(mod2)
 
 #Beyonce
+plot(ArticleBeyonceViews)
+mod3 = estimate(AR(1), ArticleBeyonceViews)
+check(mod3)
 
 #Chomsky
+plot(ArticleChomskyViews)
+mod4 = estimate(AR(1), ArticleChomskyViews)
+check(mod4)
+
+mod4 = estimate(AR(19), ArticleChomskyViews)
+check(mod4)
+
+select(AR(19), ArticleChomskyViews, include.mean = TRUE)
+
+mod4 = estimate(AR(15), ArticleChomskyViews)
+check(mod4)
+
+chomsky_mape = MAPE(AR(16), ArticleChomskyViews, start = 0.8)
 
 #Lazio
+plot(ArticleLazioViews)
+mod5 = estimate(AR(1), ArticleLazioViews)
+check(mod5)
+
+mod5 = estimate(AR(9), ArticleLazioViews)
+check(mod5)
+
+select(AR(9), ArticleLazioViews, include.mean = TRUE)
+
+Lazio_mape = MAPE(AR(9), ArticleLazioViews, start = 0.8)
+
 
 #Thanksgiving
+plot(ArticleThanksgivingViews)
+mod6 = estimate(AR(1), ArticleThanksgivingViews)
+check(mod6)
+
+mod6 = estimate(AR(3), ArticleThanksgivingViews)
+check(mod6)
+
+select(AR(3), ArticleThanksgivingViews, include.mean = TRUE)
+
+Thanksgiving_mape = MAPE(AR(3), ArticleThanksgivingViews, start = 0.8)
